e89ea5e97262fe416055f8f49555fe4e7d7ec606
lordtt13 <tanmaythakurbrn2rule@gmail.com>
2023-05-03 02:59:00 +0530

    feat: add more functions to set up broker
    Signed-off-by: lordtt13 <tanmaythakurbrn2rule@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-use eda_demo_rust::rabbit_mq_transporter::RabbitMqTransporter;
+use eda_demo_rust::{rabbit_mq_transporter::RabbitMqTransporter, mq::Transporter, packet_proto::packets::PacketDiscover};
+
 fn main() {
     // Instantiate a RabbitMqTransporter and open the connection
-    RabbitMqTransporter::new("amqp://guest:guest@localhost:5672/%2f")
-        .expect("Error creating transporter");
+    let transporter = RabbitMqTransporter::new("amqp://localhost:5672/%2f".to_string()).unwrap_or_else(
+        |e| {
+            panic!("Error creating RabbitMqTransporter: {}", e);
+        }
+    );
+
+    let discover_packet = PacketDiscover {
+        ..Default::default()
+    };
+
+    transporter.discover(discover_packet).unwrap_or_else(
+        |e| {
+            panic!("Error sending discover packet: {}", e);
+        }
+    );
+
+    transporter.subscribe_all().unwrap_or_else(
+        |e| {
+            panic!("Error subscribing to all topics: {}", e);
+        }
+    );
 }
